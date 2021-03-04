39800d174548a8d4593134603cb0badae125db9e
Kelly Ung <kelly@entale.co>
2021-03-04 21:36:07 +0000

    create UploadPostController - allow users to upload images

--- Instagram/Controller/ImageSelectorController.swift
+++ Instagram/Controller/ImageSelectorController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
+// TODO: remove
 class ImageSelectorController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemOrange
+        view.backgroundColor = .white
     }
 
 }
---
+++ Instagram/Utils/InputTextView.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class InputTextView: UITextView {
+
+}
